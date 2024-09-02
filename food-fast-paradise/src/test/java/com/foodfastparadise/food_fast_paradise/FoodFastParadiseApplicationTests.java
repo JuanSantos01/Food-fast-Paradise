906132cc4b2d5f6e8ff48f52161c67a9fc6c7c10
JuanSantos01 <juanpabloalvissantos27@gmail.com>
2024-09-01 21:56:21 -0500

    avance

---
+++ food-fast-paradise/src/test/java/com/foodfastparadise/food_fast_paradise/FoodFastParadiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodfastparadise.food_fast_paradise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodFastParadiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
